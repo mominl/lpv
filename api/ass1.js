910644b1d6ef8bc63d47837aef42052429024593
murtuza9270 <murtuza9270@gmail.com>
2026-05-10 19:17:17 +0530

    update the code

---
+++ api/ass1.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = (req, res) => {
+  const fileName = "ass1.txt";
+  const filePath = path.join(process.cwd(), "files", fileName);
+
+  if (!fs.existsSync(filePath)) {
+    return res.status(404).send("File not found");
+  }
+
+  res.setHeader("Content-Disposition", `attachment; filename="${fileName}"`);
+  res.setHeader("Content-Type", "text/plain; charset=utf-8");
+  return res.send(fs.readFileSync(filePath));
+};
