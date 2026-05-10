b118e23440e7ffe4342d639a4271cddc0ee453c9
murtuza9270 <murtuza9270@gmail.com>
2026-05-10 19:36:34 +0530

    remove the forbidden

--- api/ass1.js
+++ api/ass1.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 module.exports = (req, res) => {
   const fileName = "ass1.txt";
-  const filePath = path.join(process.cwd(), "files", fileName);
+  const filePath = path.join(__dirname, "..", "files", fileName);
 
   if (!fs.existsSync(filePath)) {
     return res.status(404).send("File not found");
